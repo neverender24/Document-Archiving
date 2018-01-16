d5481ea63ee6c3491c62c5439a7a574e4accf6dc
neverender24 <neverender24@gmail.com>
2018-01-16 17:28:41 +0800

    update drawer

--- app/Http/Controllers/InboxController.php
+++ app/Http/Controllers/InboxController.php
@@ -70,4 +70,14 @@ class InboxController extends Controller
 
     	return view('admin.document.log', compact('data'));
     }
+
+    public function destroy($id)
+    {
+        $delete = Inbox::findOrFail($id);
+        $delete->delete();
+
+        \Session::flash('success','Document Deleted');
+
+        return redirect('/inbox');
+    }
 }
